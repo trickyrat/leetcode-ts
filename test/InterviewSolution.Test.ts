13d701926e274535b66846e735587953027e0eb0
trickyrat <wangjian0513@outlook.com>
2022-09-30 03:06:58 +0800

    feature: add 面试题 01.08. Zero Matrix LCCI

--- test/InterviewSolution.Test.ts
+++ test/InterviewSolution.Test.ts
@@ -2,7 +2,32 @@ import { InterviewSolution } from "../src/InterviewSolution";
 
 let solution = new InterviewSolution;
 
+test("SetZeroesTest", () => {
+    let matrix1 = [
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 1, 1]
+    ];
+    let matrix2 = [
+        [0, 1, 2, 0],
+        [3, 4, 5, 2],
+        [1, 3, 1, 5]
+    ];
+    solution.setZeroes(matrix1);
+    solution.setZeroes(matrix2);
+    expect(matrix1).toEqual([
+        [1, 0, 1],
+        [0, 0, 0],
+        [1, 0, 1]
+    ]);
+    expect(matrix2).toEqual([
+        [0, 0, 0, 0],
+        [0, 4, 5, 0],
+        [0, 3, 1, 0]
+    ]);
+})
+
 test("IsFlippedStringTest", () => {
-    expect(solution.is_flipped_string("waterbottle", "erbottlewat")).toEqual(true);
-    expect(solution.is_flipped_string("aa", "aba")).toEqual(false);
+    expect(solution.isFlippedString("waterbottle", "erbottlewat")).toEqual(true);
+    expect(solution.isFlippedString("aa", "aba")).toEqual(false);
 })
