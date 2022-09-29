df4de9b1862f4a6aaf058af6494862a384680ac3
trickyrat <wangjian0513@outlook.com>
2022-09-30 03:02:02 +0800

    add 707. Design Linked List

---
+++ test/MyLinkedList.Test.ts
@@ -0,0 +1,13 @@
+import { MyLinkedList } from "../src/MyLinkedList";
+
+let myLinkedList = new MyLinkedList();
+
+test("MyLinkedListTest", () => {
+    myLinkedList.addAtHead(1);
+    myLinkedList.addAtTail(3);
+    myLinkedList.addAtIndex(1, 2);
+    expect(myLinkedList.get(1)).toEqual(2);
+    myLinkedList.deleteAtIndex(1);
+    expect(myLinkedList.get(1)).toEqual(3);
+})
+
