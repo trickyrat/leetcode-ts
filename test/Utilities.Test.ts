59da2edba0c3776cb1c51736b37ddc2bd380ad67
trickyrat <wangjian0513@outlook.com>
2022-10-02 15:48:58 +0800

    fix: fix utilities import issue

--- test/Utilities.Test.ts
+++ test/Utilities.Test.ts
@@ -1,12 +1,14 @@
-import { convertListNodeToArray, createListNode } from "../src/Utilities";
+import { Utilities } from "../src/Utilities";
+
+let utils = new Utilities();
 
 test("CreateListNodeTest", () => {
-  let head = createListNode([1, 2, 3, 4, 5]);
+  let head = utils.createListNode([1, 2, 3, 4, 5]);
   expect(head?.print()).toEqual("1->2->3->4->5");
 })
 
 
 test("ConvertListNodeToArrayTest", () => {
-  let head = createListNode([1, 2, 3, 4, 5]);
-  expect(convertListNodeToArray(head)).toEqual([1, 2, 3, 4, 5]);
+  let head = utils.createListNode([1, 2, 3, 4, 5]);
+  expect(utils.convertListNodeToArray(head)).toEqual([1, 2, 3, 4, 5]);
 })
