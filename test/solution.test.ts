d780c8119a59eb35225d3e0cade309e3a5434f44
trickyrat <wangjian0513@outlook.com>
2022-10-07 00:12:12 +0800

    test: add subdomainVisits test cases

--- test/solution.test.ts
+++ test/solution.test.ts
@@ -477,6 +477,12 @@ test("NumberOfLinesTest", () => {
   expect(solution.numberOfLines([4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10], "bbbcccdddaaa")).toEqual([2, 4]);
 })
 
+test("SubdomainVisitsTest", () => {
+  expect(solution.subdomainVisits(["9001 discuss.leetcode.com"])).toEqual(["9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com"]);
+  expect(solution.subdomainVisits(["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"])).toEqual(
+    ["900 google.mail.com", "901 mail.com", "951 com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org", "5 org"]);
+})
+
 test("MostCommonWordTest", () => {
   expect(solution.mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", ["hit"])).toEqual("ball");
 })
