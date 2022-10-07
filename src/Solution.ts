81f155941dc656c8895b2c928f5beac74719bece
trickyrat <wangjian0513@outlook.com>
2022-10-07 16:10:36 +0800

    feature: add 777. Swap Adjacent in LR String

--- src/Solution.ts
+++ src/Solution.ts
@@ -1542,6 +1542,48 @@ export class Solution {
     }
 
     /**
+     * 777. Swap Adjacent in LR String
+     * @param start 
+     * @param end 
+     */
+    canTransform(start: string, end: string): boolean {
+        let n = start.length;
+        let i = 0, j = 0;
+        while (i < n && j < n) {
+            while (i < n && start[i] === 'X') {
+                i++;
+            }
+            while (j < n && end[j] === 'X') {
+                j++;
+            }
+            if (i < n && j < n) {
+                let c = start[i];
+                if (c !== end[j]) {
+                    return false;
+                }
+                if ((c === 'L' && i < j) || (c === 'R' && i > j)) {
+                    return false;
+                }
+                i++;
+                j++;
+            }
+        }
+        while (i < n) {
+            if (start[i] !== 'X') {
+                return false;
+            }
+            i++;
+        }
+        while (j < n) {
+            if (end[j] !== 'X') {
+                return false;
+            }
+            j++;
+        }
+        return true;
+    }
+
+    /**
      * 793. Preimage Size of Factorial Zeroes Function
      * @param k 
      */
