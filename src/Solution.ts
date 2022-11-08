69bdc821a5f73934fbc6b75e948f167b433342f5
trickyrat <wangjian0513@outlook.com>
2022-11-09 00:21:55 +0800

    feature: add 934. Shortest Bridge

--- src/Solution.ts
+++ src/Solution.ts
@@ -2110,6 +2110,56 @@ export class Solution {
     }
 
     /**
+     * 934. Shortest Bridge
+     * @param grid 
+     */
+    shortestBridge(grid: number[][]): number {
+        let dfs = (x: number, y: number, grid: number[][], queue: [number, number][]): void => {
+            if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] != 1) {
+                return;
+            }
+            queue.push([x, y]);
+            grid[x][y] = -1;
+            dfs(x - 1, y, grid, queue);
+            dfs(x + 1, y, grid, queue);
+            dfs(x, y - 1, grid, queue);
+            dfs(x, y + 1, grid, queue);
+        };
+        const n = grid.length;
+        let dirs: number[][] = [[-1, 0], [1, 0], [0, 1], [0, -1]];
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    let queue: [number, number][] = [];
+                    dfs(i, j, grid, queue);
+                    let step = 0;
+                    while (queue.length > 0) {
+                        let size = queue.length;
+                        for (let k = 0; k < size; k++) {
+                            let cell = queue.shift()!;
+                            let x = cell[0], y = cell[1];
+                            for (let d = 0; d < 4; d++) {
+                                let nx = x + dirs[d][0];
+                                let ny = y + dirs[d][1];
+                                if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+                                    if (grid[nx][ny] === 0) {
+                                        queue.push([nx, ny]);
+                                        grid[nx][ny] = -1;
+                                    } else if (grid[nx][ny] === 1) {
+                                        return step;
+                                    }
+                                }
+                            }
+                        }
+                        step++;
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    /**
      * 940. Distinct Subsequences II
      * @param s 
      */
