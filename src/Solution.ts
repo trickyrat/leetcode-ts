82eea7700999b16f60a3446eeb76243887d65049
trickyrat <wangjian0513@outlook.com>
2022-11-13 00:31:12 +0800

    feat: add 1662. Check If Two String Arrays are Equivalent

--- src/Solution.ts
+++ src/Solution.ts
@@ -3080,6 +3080,31 @@ export class Solution {
     }
 
     /**
+     * 1662. Check If Two String Arrays are Equivalent
+     * @param word1 
+     * @param word2 
+     */
+    arrayStringAreEqual(word1: string[], word2: string[]): boolean {
+        let p1 = 0, p2 = 0, i = 0, j = 0;
+        while (p1 < word1.length && p2 < word2.length) {
+            if (word1[p1][i] !== word2[p2][j]) {
+                return false;
+            }
+            i++;
+            if (i === word1[p1].length) {
+                p1++;
+                i = 0;
+            }
+            j++;
+            if (j === word2[p2].length) {
+                p2++;
+                j = 0;
+            }
+        }
+        return p1 === word1.length && p2 === word2.length;
+    }
+
+    /**
      * 1672.Richest Customer Wealth
      * @param accounts 
      */
