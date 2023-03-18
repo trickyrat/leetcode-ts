b61fbe360f9fb7ba5aca337d90e3cfebd815db92
trickyrat <wangjian0513@outlook.com>
2023-03-19 01:30:35 +0800

    feat: add 26. Remove Duplicates from Sorted Array

--- src/Solution.ts
+++ src/Solution.ts
@@ -224,6 +224,26 @@ export class Solution {
     }
 
     /**
+     * 26. Remove Duplicates from Sorted Array
+     * @param nums 
+     */
+    removeDuplicates(nums: number[]): number {
+        const n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        let fast = 1, slow = 1;
+        while (fast < n) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+
+    /**
      * 27.Remove Element
      * @param nums 
      * @param val 
