8cd0661c978efed4247da468355f663fd2b93f78
trickyrat <wangjian0513@outlook.com>
2023-03-28 23:05:32 +0800

    feat: add 33. Search in Rotated Sorted Array

--- src/Solution.ts
+++ src/Solution.ts
@@ -260,6 +260,42 @@ export class Solution {
     }
 
     /**
+     * 33. Search in Rotated Sorted Array
+     * @param nums 
+     * @param target 
+     */
+    search(nums: number[], target: number): number {
+        const n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] === target ? 0 : -1;
+        }
+        let left = 0, right = n - 1;
+        while (left <= right) {
+            const mid = left + ((right - left) >> 1);
+            if (nums[mid] === target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
      * 35. Search Insert Position
      * @param nums 
      * @param target 
--- test/Solution.test.ts
+++ test/Solution.test.ts
@@ -101,6 +101,14 @@ test.each([
 });
 
 test.each([
+  [[4, 5, 6, 7, 0, 1, 2], 0, 4],
+  [[4, 5, 6, 7, 0, 1, 2], 3, -1],
+  [[1], 0, -1],
+])("search(%o, %i) -> %i", (nums: number[], target: number, expected: number) => {
+  expect(solution.search(nums, target)).toEqual(expected);
+});
+
+test.each([
   [[1, 3, 5, 6], 5, 2],
   [[1, 3, 5, 6], 2, 1],
   [[1, 3, 5, 6], 7, 4],
