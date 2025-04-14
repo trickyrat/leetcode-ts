2bd6b51099708906bfdfc33f8d644babc947262b
trickyrat <wangjian0513@outlook.com>
2025-04-14 20:38:42 +0800

    feat: add 5. Longest Palindromic Substring

--- src/Solution.ts
+++ src/Solution.ts
@@ -80,6 +80,35 @@ export class Solution {
         return ans;
     }
 
+    longestPalindrome(s: string): string {
+        const expandAroundCenter = (s: string, left: number, right: number): [number, number] => {
+            while (left >= 0 && right < s.length && s[left] === s[right]) {
+                --left;
+                ++right;
+            }
+
+            return [left + 1, right - 1];
+        };
+
+        if (s.length < 1) {
+            return "";
+        }
+
+        let start = 0, end = 0;
+        for (let i = 0; i < s.length; i++) {
+            const [left1, right1] = expandAroundCenter(s, i, i);
+            const [left2, right2] = expandAroundCenter(s, i, i + 1);
+            if (right1 - left1 > end - start) {
+                [start, end] = [left1, right1]; 
+            }
+            if (right2 - left2 > end - start) {
+                [start, end] = [left2, right2];
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
     /**
      * 6.Zigzag Conversion
      * @param s
